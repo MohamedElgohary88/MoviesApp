e181d729133190ae95b848f73fdc72fa88ff4e7f
mohamedelgohary <mohamed.abdelazim.elgohary@gmail.com>
2024-11-09 22:35:50 +0200

    create app router

--- lib/app_router.dart
+++ lib/app_router.dart
@@ -1,16 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:movies_app/data/web_services/characters_web_services.dart';
 import 'package:movies_app/presentation/screens/character_details.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:movies_app/presentation/screens/characters_screen.dart';
 import 'constants/strings.dart';
+import 'data/repository/characters_repository.dart';
+import 'domain/characters_cubit.dart';
 
 class AppRouter {
+  late CharactersRepository charactersRepository;
+  late CharactersCubit charactersCubit;
+
+  AppRouter() {
+    charactersRepository = CharactersRepository(CharactersWebServices());
+    charactersCubit = CharactersCubit(charactersRepository);
+  }
+
   Route? generateRoute(RouteSettings settings) {
     switch (settings.name) {
-      case charactersScreen :
-        return MaterialPageRoute(builder: (_) => const CharactersScreen());
+      case charactersScreen:
+        return MaterialPageRoute(
+            builder: (_) => BlocProvider(
+                  create: (BuildContext context) => charactersCubit,
+                  child: CharactersScreen(),
+                ));
 
-        case characterDetailsScreen :
-        return MaterialPageRoute(builder: (_) => const CharacterDetailsScreen());
+      case characterDetailsScreen:
+        return MaterialPageRoute(
+            builder: (_) => const CharacterDetailsScreen());
     }
   }
 }
--- lib/presentation/screens/characters_screen.dart
+++ lib/presentation/screens/characters_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+
 class CharactersScreen extends StatelessWidget {
   const CharactersScreen({Key? key}) : super(key: key);
 
